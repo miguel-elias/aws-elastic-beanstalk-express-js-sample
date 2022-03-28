fbea5c4d8608587b27d5d984bfd724f709163480
Miguel Elias <elias.msalomao.miguel599@gmail.com>
2022-03-28 17:30:21 -0300

    add alteração Miguel

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Miguel Elias'));
+//app.get('/', (req, res) => res.send('Hello World! Miguel Elias'));
 
 app.get('/', (req, res) => res.send('Deus é bom o tempo todo, o tempo todo Deus é bom'));
 
